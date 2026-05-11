f8120b7306be9c86014a094561d5dcda38ea7069
Maria Virginia Souza <mariavirginiasouza02@gmail.com>
2026-05-11 10:12:40 -0300

    migração do banco sqlite para supabase

--- kindercare/settings.py
+++ kindercare/settings.py
@@ -1,5 +1,9 @@
 from pathlib import Path
 import os
+import dj_database_url
+from dotenv import load_dotenv
+
+load_dotenv()
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -55,10 +59,11 @@ WSGI_APPLICATION = 'kindercare.wsgi.application'
 ASGI_APPLICATION = 'kindercare.asgi.application'
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default=os.getenv('DATABASE_URL'),
+        conn_max_age=600,
+        ssl_require=True
+    )
 }
 
 AUTH_PASSWORD_VALIDATORS = [
